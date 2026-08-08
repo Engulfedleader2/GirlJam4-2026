e3667d998bc9c9a8091d803bcfe48e4df29dc8c2
Israel <engulfedleader@gmail.com>
2026-08-08 14:34:25 -0400

    Refactor/Organizatrion
    Moved core managers script into their own sub folder + added project autoload

---
+++ Scripts/Managers/AudioManager.cs
@@ -0,0 +1,58 @@
+using Godot;
+using System;
+
+public class AudioManager : Node
+{
+	public static AudioManager Instance { get; private set; }
+
+	private AudioStreamPlayer musicPlayer;
+	private AudioStreamPlayer sfxPlayer;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Instance = this;
+
+		musicPlayer = new AudioStreamPlayer { Name = "MusicPlayer" };
+		AddChild(musicPlayer);
+
+		sfxPlayer = new AudioStreamPlayer { Name = "SFXPlayer" };
+		AddChild(sfxPlayer);
+	}
+
+	public void PlayMusic(AudioStream music)
+	{
+		if (music == null)
+		{
+			return;
+		}
+
+		musicPlayer.Stream = music;
+		musicPlayer.Play();
+	}
+	public void StopMusic()
+	{
+		musicPlayer.Stop();
+	}
+
+	public void PlaySFX(AudioStream sfx)
+	{
+		if (sfx == null)
+		{
+			return;
+		}
+
+		sfxPlayer.Stream = sfx;
+		sfxPlayer.Play();
+	}
+
+	public void SetMusicVolume(float volumeDb)
+	{
+		musicPlayer.VolumeDb = volumeDb;
+	}
+
+	public void SetSFXVolume(float volumeDb)
+	{
+		 sfxPlayer.VolumeDb = volumeDb;
+	}
+}
--- Scripts/Managers/SceneManager.cs
+++ Scripts/Managers/SceneManager.cs
@@ -3,19 +3,14 @@ using System;
 
 public class SceneManager : Node
 {
-	public static SceneManager Instance { get; private set; }
 
 	private const string MainMenuScenePath = "res://Scenes/MainMenu.tscn";
+	private const string MainGameScenePath = "res://Scenes/MainGame.tscn";
 	private const string SettingsMenuScenePath = "res://Scenes/SettingsMenu.tscn";
 	private const string PauseMenuScenePath = "res://Scenes/PauseMenu.tscn";
 	private const string LeaderboardScenePath = "res://Scenes/Leaderboard.tscn";
+	private const string CreditsMenuScenePath = "res://Scenes/CreditsMenu.tscn";
 
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		Instance = this;
-	}
 
 	public void ChangeScene(string scenePath)
 	{
@@ -32,6 +27,11 @@ public class SceneManager : Node
 	{
 		ChangeScene(MainMenuScenePath);
 	}
+
+	public void GoToMainGame()
+	{
+		ChangeScene(MainGameScenePath);
+	}
 	
 	public void GoToPauseMenu()
 	{
@@ -42,14 +42,18 @@ public class SceneManager : Node
 	{
 		ChangeScene(SettingsMenuScenePath);
 	}
+	public void GoToCreditsMenu()
+	{
+		ChangeScene(CreditsMenuScenePath);
+	}
 
-	public void goToLeaderboard()
+	public void GoToLeaderboard()
 	{
 		//TODO: This might need a DB hook up if we decide to go this route
 		ChangeScene(LeaderboardScenePath);
 	}
 
-	public void quitGame()
+	public void QuitGame()
 	{
 		GetTree().Quit();
 	}
