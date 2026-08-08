45a50d7d3e5def1b62ef19a59a5fd333c9ac1bef
Israel <engulfedleader@gmail.com>
2026-08-08 14:48:48 -0400

    Fixed some merge conflitcs

--- Scripts/UI/CreditsMenu.cs
+++ Scripts/UI/CreditsMenu.cs
@@ -1,55 +1,8 @@
 using Godot;
-using System;
 
 public class CreditsMenu : Node
 {
-    public static AudioManager Instance { get; private set; }
-
-    private AudioStreamPlayer musicPlayer;
-    private AudioStreamPlayer sfxPlayer;
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        Instance = this;
-
-        musicPlayer = GetNode<AudioStreamPlayer>("MusicPlayer");
-        sfxPlayer = GetNode<AudioStreamPlayer>("SFXPlayer");
-    }
-
-    public void PlayMusic(AudioStream music)
-    {
-        if (music == null)
-        {
-            return;
-        }
-
-        musicPlayer.Stream = music;
-        musicPlayer.Play();
-    }
-    public void StopMusic()
-    {
-        musicPlayer.Stop();
-    }
-
-    public void PlaySFX(AudioStream sfx)
-    {
-        if (sfx == null)
-        {
-            return;
-        }
-
-        sfxPlayer.Stream = sfx;
-        sfxPlayer.Play();
-    }
-
-    public void SetMusicVolume(float volumeDb)
-    {
-        musicPlayer.VolumeDb = volumeDb;
-    }
-
-    public void SetSFXVolume(float volumeDb)
-    {
-         sfxPlayer.VolumeDb = volumeDb;
-    }
+	public override void _Ready()
+	{
+	}
 }
