8d4f50a1e2fb812d2fab8e5744f776ed385f8a13
Kevin <k.horkaew2@gmail.com>
2026-08-12 18:13:33 -0400

    ComicPanel cutscene
    Added comic panel scene

---
+++ Scenes/Comic/ComicPanel.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+
+var can_play = false
+
+var index = 1
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+
+func _process(delta: float) -> void:
+	if can_play == true:
+		_pause()
+		whatScene()
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_pressed("ui_accept"):
+		index += 1
+		_play()
+
+
+func whatScene():
+	if index == 2:
+		$AnimationPlayer.play("cam_2")
+	if index == 3:
+		$AnimationPlayer.play("cam_3")
+	if index == 4:
+		$AnimationPlayer.play("cam_4")
+	if index == 5:
+		$AnimationPlayer.play("cam_5")
+	if index == 6:
+		$AnimationPlayer.play("cam_6")
+
+
+func _play():
+	can_play = true
+
+func _pause():
+	can_play = false
