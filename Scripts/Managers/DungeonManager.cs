c093dc6cfdbba0c857ed4da990a980801c76c956
Roo <clarathle@gmail.com>
2026-08-13 18:22:25 -0400

    resolving git issues which was erasing my updates

--- Scripts/Managers/DungeonManager.cs
+++ Scripts/Managers/DungeonManager.cs
@@ -15,7 +15,6 @@ public class DungeonManager : Node
 	public List<Adventurer> Party { get; private set; } = new List<Adventurer>();
 
 	public int CurrentFloor { get; private set; }
-	public int GoldEarned { get; private set; }
 	public bool IsRunActive { get; private set; }
 
 	
@@ -131,7 +130,6 @@ public class DungeonManager : Node
 		Party = new List<Adventurer>(party);
 
 		CurrentFloor = 1;
-		GoldEarned = 0;
 		IsRunActive = true;
 	}
 
@@ -200,7 +198,6 @@ public class DungeonManager : Node
 	private void GiveFloorReward()
 	{
 		GameManager.Instance.AddTreasure(GoldPerFloor);
-		GoldEarned += GoldPerFloor;
 	}
 	/*
 	public void EndRun()
