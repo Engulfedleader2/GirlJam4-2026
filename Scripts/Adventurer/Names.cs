0213e7fd4516d71cb3c1b96f247fafe90ad72d03
Israel <engulfedleader@gmail.com>
2026-08-14 23:21:52 -0400

    Minor Updates

---
+++ Scripts/Adventurer/Names.cs
@@ -0,0 +1,40 @@
+// Name pool for newly generated adventurers.
+
+public static class Names
+{
+	public static readonly string[] All =
+	{
+		"Adam", "Allen", "Abby", "Ashely", "Anton", "Albert", "Adrian", "Aghast", "Anchovy", "Ahmed",
+		"Billy", "Bobby", "Big", "Boot", "Buster", "Bestee", "Beck", "Blondie", "Boss", "Bundt",
+		"Carl", "Charlie", "Cass", "Cave", "Celeste", "Coast", "Cain", "Cutter", "Cain", "Cub",
+		"Dave", "Dillian",
+		"Dobby", "Doer", "Derek", "Dustin", "Demetry", "Dusk", "Dawn", "Depth",
+		"Eric", "Elaine", "Esther", "Evan", "Ethan", "Elanor", "Eternal", "Egg", "Ebb", "Eddy",
+		"Frank", "Foss", "Fly", "Flake", "Fedrick", "Fievel", "Felicia", "Fiesta", "Festival", "Fuu",
+		"Gang", "Garrick", "Gus",
+		"Gore", "Greg", "Girl", "Gain", "Grok", "Gone", "Guy",
+		"Harry", "Henry", "Harriot", "Howard", "Harrow", "Howdy", "Host", "Heck", "Hill", "Hubby",
+		"Issac", "Indy", "Iggy", "Isle", "Ichi", "Ivy", "Ivan", "Illinoi", "Italic", "Idle",
+		"Jack", "John", "Jill", "Jess",
+		"Jim", "Justin", "Jupe", "Jai", "Jeff", "Joy",
+		"Kev", "Korn", "Kris", "Kass", "Konroy", "Kyle", "Kismet", "Kiss", "Kitch", "Kai",
+		"Love", "Long", "Lone", "Lee", "Lest", "Lack", "Luster", "Labubu", "Lou", "Letty",
+		"Moss", "Mother", "Milk", "Messi", "Mister",
+		"Misses", "Mal", "Mole", "Must", "Mold",
+		"Ness", "Neil", "Numb", "Nun", "Nikki", "Nest", "Null", "Nutt", "Nose", "Nuggs",
+		"Owen", "Over", "Otter", "Ostrich", "Osteen", "Oligarch", "Obi", "Oden", "Orwell", "Oger",
+		"Peter", "Patrick", "Patrol", "Pollie", "Pop", "Pill",
+		"Pest", "Pal", "Patrice", "Pongo",
+		"Queen", "Quinn", "Quest", "Quick", "Quitter", "QQ", "Quentin", "Quincy", "Quinta", "Quaid",
+		"Ryan", "Riley", "Racer", "Rail", "Rain", "Rot", "Rust", "Rake", "Ron", "Rob",
+		"Sam", "Snail", "Snake", "Sim", "Steph", "Steve", "Stall",
+		"Stark", "Stan", "Style",
+		"Tie", "Tim", "Tyke", "Tron", "Tusk", "Task", "Test", "Till", "Thy", "Top",
+		"Ulysses", "Ubo", "Udon", "Ube", "Uma", "Uwe", "Uno", "Ulrich", "Ursala", "Uzo",
+		"Vicky", "Vinnie", "Val", "Vest", "View", "Von", "Vincent", "Valentine",
+		"Vin", "Viggo",
+		"Won", "Win", "Wyatt", "West", "Whack", "Will", "War", "Wolf", "Weave", "Wood",
+		"Xavier", "Xtreme", "Xander", "Xolo", "Xi", "Xoshee", "Xue", "Xin", "Ximba", "XingXu",
+		"Zen", "Zoe", "Zach", "Zoo", "Zelda", "Zelko", "Zeb", "Zanadu", "Zimbob", "Zombee"
+	};
+}
