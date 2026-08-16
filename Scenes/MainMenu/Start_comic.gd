2c9bebcc848d91767705e2a904915e559428b7f7
Roo <clarathle@gmail.com>
2026-08-16 09:48:13 -0400

    [roo] fixing where things are pointing since there were different imports

--- Scenes/MainMenu/Start_comic.gd
+++ Scenes/MainMenu/Start_comic.gd
@@ -1,6 +1,6 @@
 extends Button
 
-var start_game_sfx = preload("res://Assets/Audio/SFX/SFX_UI_Start_Game.wav")
+var start_game_sfx = preload("res://Assets/Audio/SFX/UI/SFX_UI_Start_Game.wav")
 
 
 func _on_Start_comic_pressed():
